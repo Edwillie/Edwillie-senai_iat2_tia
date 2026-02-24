7d30483bb8d413e81ebd3f47b36493bae88b70b8
Edwillie <edwillie@gmail.com>
2026-02-23 21:46:11 -0300

    Fim de aula

--- Aula05/exemplo05.py
+++ Aula05/exemplo05.py
@@ -2,30 +2,30 @@ import numpy as np
 import skfuzzy as fuzz
 import requests
 
-x = np.linspace(15, 35, 200)
-agradavel = fuzz.trimf(x, [18, 24, 30])
+x = np.linspace(0, 100, 500)
+agradavel = fuzz.trimf(x, [30, 50, 70])
 
-cidade = "Pindamonhangaba"
+cidade = input("Informe o nome da cidade: ")
 url = f"http://wttr.in/{cidade}?format=j1"
 
 reposta = requests.get(url)
 dados = reposta.json()
-temp_real = float(dados["current_condition"][0]["temp_C"])
+humidity_real = float(dados["current_condition"][0]["humidity"])
 
-grau = fuzz.interp_membership(x, agradavel, temp_real)
+grau = fuzz.interp_membership(x, agradavel, humidity_real)
 
 print("Cidade: ", cidade)
-print("Temperatura Real: ", temp_real)
+print("Umidade Real: ", humidity_real)
 print("Grau: ", round(grau, 2))
 
 if grau == 1.0:
-    acao = "Perfeito, Ar em espera"
+    acao = "Excelente Umidade"
 elif grau >= 0.5:
-    acao = "Razoavel, Diminuir potencia do ar"
+    acao = "Baixa Umidade"
 elif grau >= 0.0:
-    acao = "Quase fora. Aumentar potencia do ar"
+    acao = "Umidade acima do esperado"
 else:
-    acao = "Fora do conforto! Ligar o ar no máximo!"
+    acao = "Umidade fora de padrão"
 
 print("Ação: ", acao) 
 
